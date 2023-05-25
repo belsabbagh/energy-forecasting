b39e606fd33903ea5d5d373f0e58f5c787a3de7e
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2023-05-25 11:11:12 +0300

    updated paths

--- visualize_data.py
+++ visualize_data.py
@@ -2,11 +2,11 @@ from os import path
 from matplotlib import pyplot as plt
 from src import dataset_handler
 
-CHARTS_PATH = path.join("data", "charts")
+CHARTS_PATH = path.join("out", "charts")
 
 for country, df in dataset_handler.country_iter():
     df.dropna()
     df.plot()
     plt.savefig(path.join(CHARTS_PATH, f"{country}.png"), dpi=300)
     plt.close()
-    print(f'Visualized {country}')
+    print(f"Visualized {country}")
