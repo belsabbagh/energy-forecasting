0f530c6cd4000d1e7c55d2829a951f218ecb592c
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2023-05-26 17:10:54 +0300

    visualization changes

--- analyze_data.py
+++ analyze_data.py
@@ -1,5 +1,11 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+
+from statsmodels.tsa.seasonal import seasonal_decompose
 from src import dataset_handler
 
 
-for country, df in dataset_handler.country_iter():
-    df.corr(numeric_only=True).to_csv(f"out/analysis/correlation/{country}.csv")
+if __name__ == "__main__":
+    for country, df in dataset_handler.country_iter():
+        print(f"Analyzing {country}... ", end="")
+        df.corr(numeric_only=True).to_csv(f"out/analysis/correlation/{country}.csv")
--- visualize_data.py
+++ visualize_data.py
@@ -1,15 +1,27 @@
 from os import path
 from matplotlib import pyplot as plt
+import pandas as pd
 from src import dataset_handler
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 CHARTS_PATH = path.join("out", "charts")
 
+
+def plot_patterns(df: pd.DataFrame):
+    for r in df.columns:
+        plot = seasonal_decompose(df[[r]], period=2, model="additive").plot()
+        plot.savefig(path.join(CHARTS_PATH, "patterns", f"{country}_{r}.png"))
+        plt.close()
+    return
+
+
 for country, df in dataset_handler.country_iter():
-    df.dropna()
+    df.dropna(inplace=True)
+    plot_patterns(df)
     plot = df.plot(ylim=(0, None))
     plot.set_ylabel("Energy (Quadrillion BTU)")
     plot.set_xlabel("Year")
     plot.legend(loc="upper right", fontsize=7)
-    plt.savefig(path.join(CHARTS_PATH, f"{country}.png"), dpi=300)
+    plt.savefig(path.join(CHARTS_PATH, "overviews", f"{country}.png"), dpi=300)
     plt.close()
     print(f"Visualized {country}")
